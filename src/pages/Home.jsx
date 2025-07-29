6d643adbc11d060fb7e47f5015c751b210bb26d2
ajy15 <andrewyansick@gmail.com>
2025-07-28 22:28:16 -0400

    Setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '4rem' }}>
+      <h1>Hi, I'm Andrew Yansick</h1>
+      <p>Computer Science Student | Aspiring Software Engineer</p>
+    </div>
+  );
+}
+
+export default Home;
